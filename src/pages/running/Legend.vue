309f9ed69d0d0e6c23f3e3bebcb4420f7fd757e1
mzb0312@163.com <moluren0312>
2019-01-23 16:26:00 +0800

    1

--- src/pages/running/Legend.vue
+++ src/pages/running/Legend.vue
@@ -3,8 +3,67 @@
     <div class="legend-title">
       图例
     </div>
-    <div>
-
+    <div style="padding: 10px;font-size: 14px">
+      <Row>
+        <Col span="8">
+        <div class="legend-graph">
+          <div style="width: 40px;height: 5px;background-color: #6CFF00"></div>
+        </div>
+        </Col>
+        <Col span="16">
+        <div>畅&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;通</div>
+        </Col>
+      </Row>
+      <Row style="margin-top: 5px">
+        <Col span="8">
+        <div class="legend-graph">
+          <div style="width: 40px;height: 5px;background-color: #F0E800"></div>
+        </div>
+        </Col>
+        <Col span="16">
+        <div>缓&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;行</div>
+        </Col>
+      </Row>
+      <Row style="margin-top: 5px">
+        <Col span="8">
+        <div class="legend-graph">
+          <div style="width: 40px;height: 5px;background-color: #FF6600"></div>
+        </div>
+        </Col>
+        <Col span="16">
+        <div>拥&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;挤</div>
+        </Col>
+      </Row>
+      <Row style="margin-top: 5px">
+        <Col span="8">
+        <div class="legend-graph">
+          <div style="width: 40px;height: 5px;background-color: #D20000"></div>
+        </div>
+        </Col>
+        <Col span="16">
+        <div>严重拥堵</div>
+        </Col>
+      </Row>
+      <Row style="margin-top: 5px">
+        <Col span="8">
+        <div class="legend-graph">
+          <Icon type="alert-circled" size="18" color="#FE4040"></Icon>
+        </div>
+        </Col>
+        <Col span="16">
+        <div>突发事件</div>
+        </Col>
+      </Row>
+      <Row style="margin-top: 5px">
+        <Col span="8">
+        <div class="legend-graph">
+          <Icon type="flag" size="18" color="#F1C600"></Icon>
+        </div>
+        </Col>
+        <Col span="16">
+        <div>道路施工</div>
+        </Col>
+      </Row>
     </div>
   </div>
 </template>
@@ -25,8 +84,8 @@
 </script>
 <style scoped>
   .legend {
-    height: 150px;
-    width: 150px;
+    height: 191px;
+    width: 140px;
     position: absolute;
     cursor: pointer;
     left: 378px;
@@ -43,6 +102,14 @@
     font-size: 16px;
     display: flex;
     justify-content: flex-start;
+    align-items: center;
+  }
+
+  .legend .legend-graph {
+    width: 40px;
+    height: 21px;
+    display: flex;
+    justify-content: center;
     align-items: center;
   }
 </style>
