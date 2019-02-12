be3d3aa4d10155f4d9ce9c1cddb2910e963e4146
maozebing <mzb0312@163.com>
2019-02-12 16:57:03 +0800

    1

--- src/pages/running/Video.vue
+++ src/pages/running/Video.vue
@@ -2,9 +2,15 @@
   <div class="video">
     <div class="video-control-button" v-show="!showVideoList" @click="showVideoList=true"></div>
     <div class="video-list" v-show="showVideoList">
-      <Tabs type="card">
-        <Tab-pane label="视频列表">
-
+      <Tabs type="card" class="video-list-card">
+        <Tab-pane label="视频列表" class="video-list-card">
+          <div class="video-list-content" :style="{height: contentHeight+'px'}">
+            <div style="position:relative" v-for="item in videoData">
+              <img style="width: 280px;height: 150px" :src="item.videoPicture"/>
+              <img class="video-list-content-play" src="../../../static/images/play.png" @click="playVideo(item)"/>
+              <div class="video-list-content-title">{{item.name}}</div>
+            </div>
+          </div>
         </Tab-pane>
         <Button @click="showVideoList=false" size="small" slot="extra">收起</Button>
       </Tabs>
@@ -12,19 +18,59 @@
   </div>
 </template>
 <script>
+  import {api_getVideos} from '../../axios/api/traffic_api'
 
   export default {
     components: {},
     data() {
       return {
-        showVideoList: false
+        contentHeight: 0,
+        showVideoList: false,
+        videoData: []
       }
     },
-    methods: {},
+    methods: {
+      refreshTabPannelHeight() {
+        this.contentHeight = this.$parent.$parent.$el.offsetHeight - 98;
+      },
+      initData() {
+        api_getVideos({
+          pageIndex: 1
+        }).then(res => {
+          //格式化时间
+          res.data.forEach(v => {
+            if (new Date(v.occtime) >= this.$moment(new Date()).startOf('day')) {
+              v.time = this.$moment(new Date(v.occtime)).format("HH:mm")
+            } else if (new Date(v.occtime) >= this.$moment(new Date()).subtract(1, 'days').startOf('day')) {
+              v.time = '昨天'
+            } else if (new Date(v.occtime) >= this.$moment(new Date()).subtract(2, 'days').startOf('day')) {
+              v.time = '前天'
+            } else {
+              v.time = '更早'
+            }
+          });
+          //时间排序
+          res.data.sort(function (a, b) {
+            return new Date(a.occtime) < new Date(b.occtime) ? 1 : -1;
+          });
+          //赋值
+          this.videoData = res.data;
+        }).catch(err => {
+        });
+      },
+      playVideo(item) {
+        console.log(item)
+      }
+    },
     mounted: function () {
+      this.$root.$on('fullScreen', this.refreshTabPannelHeight);
       this.$nextTick(function () {
-
+        this.refreshTabPannelHeight()
+        this.initData()
       })
+    },
+    beforeDestroy() {
+      this.$root.$off('fullScreen');
     }
   }
 </script>
@@ -50,6 +96,35 @@
     background-color: rgba(255, 255, 255, 0.8);
     z-index: 10000;
   }
+
+  .video .video-list-card {
+    height: calc(100%);
+  }
+
+  .video .video-list-content {
+    padding: 0px 5px 0px 5px;
+    height: calc(100%);
+    overflow-y: auto;
+  }
+
+  .video .video-list-content-play {
+    width: 64px;
+    height: 64px;
+    position: absolute;
+    top: 43px;
+    left: 108px;
+  }
+
+  .video .video-list-content-title {
+    color: #ffffff;
+    text-align: left;
+    font-size: 14px;
+    width: 280px;
+    position: absolute;
+    bottom: 5px;
+    left: 5px;
+  }
+
 </style>
 <style>
   .ivu-tabs-bar {
