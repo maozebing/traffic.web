9876a1661e34529c101600d47c1ca33413fe8685
maozebing <mzb0312@163.com>
2019-02-13 12:55:11 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
-import store from './store/store'
 import moment from 'moment'
 import constant from './common/constant/constant'
 
@@ -18,7 +18,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  store,
   components: { App },
   template: '<App/>'
 })
